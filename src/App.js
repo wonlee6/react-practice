0d3b988da0c39c8cfa1aea82fdbde444f6afd799
wonlee6 <wonlee6@gmail.com>
2020-12-11 20:41:49 +0900

    props, state, Event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say';
+import EventPractice from './EventPractice';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// props : 컴포넌트 속성 , 부모 컴포넌트에서 설정
+// state : 컴포넌트 내부에서 바뀔수 있는 값 (클래스형 state, 함수형 setState), 컴포넌트 내부에서 설정
+
+const App = () => {
+  return <div><MyComponent name="REACT" favoriteNumber={1}>리액트</MyComponent>
+    <Counter />
+    <Say />
+    <EventPractice />
+  </div>
 }
+
+
 
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0,
+            fixnumber: 0
+        }
+    }
+    render() {
+        const { number, fixnumber } = this.state;
+        return (
+            <div>
+                <h1>{number}</h1>
+                <button onClick={() => {
+                    this.setState({
+                        number: number + 1
+                    },
+                        () => {
+                            console.log('setState 호출');
+                            console.log(this.state);
+                        })
+                }}
+                > + 1</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
