5e66c31b5fb622caacccf7ce0b414109775ba3b2
wonlee6 <wonlee6@gmail.com>
2020-12-15 05:00:38 +0900

    axios, usePromis 커스텀 Hook 만들기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,27 +1,46 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import axios from 'axios';
+import NewsList from './component/NewsList';
+import Categories from './component/Categories';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
 
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-      setData(response.data);
-    } catch (e) {
-      console.log(e)
-    }
-  };
+  // NewsPage.js 에서 기능 수행
+  // const [category, setCategory] = useState('all');
+  // const onSelect = useCallback(category => setCategory(category), []);
 
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
 
-  );
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       'http://newsapi.org/v2/top-headlines?country=kr&apiKey=0535ad9d7107451db75d8dba29ce4579');
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e)
+  //   }
+  // };
+
+  return <Route path="/:category?" component={NewsPage} /> // /:category? = 선택적이다. 있을수도있고 없을수도있다
+
+  // (
+  //   <ㄱ>
+  //     <Categories category={category} onSelect={onSelect} />
+  //     <NewsList category={category} />
+  //   </ㄱ>
+  // )
+
+
+  // <div>
+  //   <div>
+  //     <button onClick={onClick}>불러오기</button>
+  //   </div>
+  //   {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+  // </div>
+
+
 };
 
 export default App;
