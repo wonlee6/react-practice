2e49a52beef7550f3d697b6e85d85490dc18f9b3
wonlee6 <wonlee6@gmail.com>
2020-12-15 03:33:53 +0900

    router 기능 예제 실습

---
+++ router-tutorial/src/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import WithRouterSample from './WithRouterSample';
+
+const data = {
+    velopert: {
+        name: '이상원',
+        description: '리액트를 좋아하는 사람'
+    },
+    gildong: {
+        name: '홍길동',
+        description: '고전 소설 홍길동전 주인공'
+    }
+}
+const Profile = ({ match }) => {
+
+    const { username } = match.params;
+    const profile = data[username];
+    if (!profile) {
+        return <div>존재하지 않는 사람 입니다.</div>
+    }
+    return (
+        <div>
+            <h3>{username}({profile.name})</h3>
+            <p>{profile.description}</p>
+            <WithRouterSample />
+        </div>
+    );
+};
+
+export default withRouter(Profile);
