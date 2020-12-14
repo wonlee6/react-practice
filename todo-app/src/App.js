38ebccf339320d782980f3991b38444dfc5fd57a
wonlee6 <wonlee6@gmail.com>
2020-12-14 21:17:53 +0900

    useState, memo, virtualized

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,25 +3,21 @@ import TodoTemplate from './component/TodoEmplate';
 import TodoInsert from './component/TodoInsert';
 import TodoList from './component/TodoList';
 
+function createBulkTodo() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    })
+  }
+  return array;
+}
+
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일 해보기',
-      checked: true
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false
-    }
-  ])
+  const [todos, setTodos] = useState(createBulkTodo);
 
   // useState가 아닌 useRef를 사용하는 이유 :  id값은 렌더링 되는 정보가 아니기 때문
   //고유값으로 사용될 id
@@ -34,21 +30,21 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      setTodos(todos => todos.concat(todo));
       nextId.current += 1;
-    }, [todos],
+    }, [],
   );
 
   const onRemove = useCallback(id => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  }, [todos])
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }, [])
 
   const onToggle = useCallback(id => {
-    setTodos(todos.map(todo =>
+    setTodos(todos => todos.map(todo =>
       todo.id === id ? { ...todo, checked: !todo.checked } : todo,
     )
     )
-  }, [todos],
+  }, [],
   )
   return (
     <TodoTemplate>
--- todo-app/src/component/TodoListItem.js
+++ todo-app/src/component/TodoListItem.js
@@ -3,21 +3,22 @@ import './TodoListItem.css';
 import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => { // props 로 통째로 전달해야 최적화 할 때 편리
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => { // props 로 통째로 전달해야 최적화 할 때 편리
     const { id, text, checked } = todo;
 
     return (
-
-        <div className="TodoListItem">
-            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className="text">{text}</div>
-            </div>
-            <div className="remove" onClick={() => onRemove(id)}>
-                <MdRemoveCircleOutline />
-            </div>
-        </div >
+        <div className="TodoListItem-virtualized" style={style}>
+            <div className="TodoListItem">
+                <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+                    {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                    <div className="text">{text}</div>
+                </div>
+                <div className="remove" onClick={() => onRemove(id)}>
+                    <MdRemoveCircleOutline />
+                </div>
+            </div >
+        </div>
     );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem, (prevProps, nextProps) => prevProps.todo === nextProps.todo);
