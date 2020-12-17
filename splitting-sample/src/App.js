9fc96e761e8677279a27a6bd09d498693a6be510
wonlee6 <wonlee6@gmail.com>
2020-12-17 22:27:52 +0900

    Loadable, React.lazy

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
+import React, { Suspense, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import loadable from '@loadable/component';
 
-class App extends Component {
-  state = {
-    SplitMe: null
-  };
-  handleClick = async () => {
-    const loadedModule = await import('./SplitMe');
-    this.setState({
-      SplitMe: loadedModule.default
-    })
-  }
-  render() {
-    const { SplitMe } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>Hello react</p>
-          {SplitMe && <SplitMe />}
-        </header>
+//const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>Loading...</div>
+});
 
-      </div>
-    );
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
   }
-}
+  const onMouseOver = () => {
+    SplitMe.preload();
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick} onMouseOver={onMouseOver}>Heloow wowowo</p>
+        {/* <Suspense fallback={<div>Loading ....</div>}> */}
+        {visible && <SplitMe />}
+        {/* </Suspense> */}
+      </header>
+
+    </div >
+  );
+};
 
 export default App;
