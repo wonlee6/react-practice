cf370e0c9740291db2d847bdf54099e77c1fcb1b
wonlee6 <wonlee6@gmail.com>
2020-12-17 22:16:50 +0900

    class splitting

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,31 +1,30 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// 상단 import 로 하면 메인에 포함되지만 
-// 메소드 안 import로 하면 따로 build되어 저장된다.
-function App() {
-  const onClick = () => {
-    import('./notify').then(result => result.default());
+class App extends Component {
+  state = {
+    SplitMe: null
+  };
+  handleClick = async () => {
+    const loadedModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadedModule.default
+    })
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p onClick={onClick}>Heeeeeeeee</p>
-      </header>
-    </div>
-  );
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>Hello react</p>
+          {SplitMe && <SplitMe />}
+        </header>
+
+      </div>
+    );
+  }
 }
 
 export default App;
