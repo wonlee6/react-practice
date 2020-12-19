4795260b56a6c0108acc5797eadb41d0573054d2
wonlee6 <wonlee6@gmail.com>
2020-12-20 02:50:24 +0900

    mongoDB 연결

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
 // next 함수는 Promise 반환
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+//비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const {PORT, MONGO_URL} = process.env;
+
+mongoose
+.connect(MONGO_URL, {useNewUrlParser: true, useFindAndModify: false})
+.then(() => {
+    console.log('connected to MongoDB');
+})
+.catch(e => {
+    console.log(e)
+})
 
 const api = require('./api');
 const app = new Koa();
@@ -50,7 +64,7 @@ app.use(router.routes()).use(router.allowedMethods());
 // app.use(ctx => {
 //     ctx.body = 'hello world';
 // });
-
-app.listen(4000, () => {
-  console.log('listening to port 4000');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('listening to port %d', port);
 });
