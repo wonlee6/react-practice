68cea5aa10974e18f3e4b5b276d2629f0587f7ac
wonlee6 <wonlee6@gmail.com>
2020-12-23 03:20:17 +0900

    frontend

---
+++ blog/blog-frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+
+
+const App = () => {
+  return (
+    <>
+      {/* path에 배열을 넣어주면 한 라우트 컴포넌트에 여러 개의 경로를 쉽게 설정 가능 */}
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path={'/login'} />
+      <Route component={RegisterPage} path={'/register'} />
+      <Route component={WritePage} path={'/write'} />
+      <Route component={PostPage} path={'/@:username/:postId'} />
+    </>
+  );
+};
+
+export default App;
