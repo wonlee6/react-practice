fd964c61ce1e321a753515adf87db604944fa122
wonlee6 <wonlee6@gmail.com>
2020-12-25 20:15:10 +0900

    PostPage wrtie

--- blog/blog-frontend/src/components/post/PostViewer.js
+++ blog/blog-frontend/src/components/post/PostViewer.js
@@ -49,25 +49,38 @@ const PostContent = styled.div`
   color: ${palette.gray[8]};
 `;
 
-const PostViewer = () => {
+const PostViewer = ({ post, error, loading }) => {
+  // 에러 발생시
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <PostViewerBlock>존재 하지 않는 포스트입니다.</PostViewerBlock>;
+    }
+    return <PostViewerBlock>오류 발생</PostViewerBlock>;
+  }
+
+  // 로딩 중이거나 아직 포스트 데이터가 없을 때
+  if (loading || !post) {
+    return null;
+  }
+  const { title, body, user, publishedDate, tags } = post;
   return (
     <PostViewerBlock>
       <PostHead>
-        <h1>제목</h1>
+        <h1>{title}</h1>
         <SubInfo>
           <span>
-            <b>Tester</b>
+            <b>{user.username}</b>
           </span>
-          <span>{new Date().toLocaleDateString()}</span>
+          <span>{new Date(publishedDate).toLocaleDateString()}</span>
         </SubInfo>
         <Tags>
-          <div className="tag">#태그1</div>
-          <div className="tag">#태그2</div>
-          <div className="tag">#태그3</div>
+          {tags.map((tag) => (
+            <div className="tag">#{tag}</div>
+          ))}
         </Tags>
       </PostHead>
       <PostContent
-        dangerouslySetInnerHTML={{ __html: '<p>HTML <b>내용</b>입니다.</p>' }} // html 적용하기위해
+        dangerouslySetInnerHTML={{ __html: body }} // html 적용하기위해
       />
     </PostViewerBlock>
   );
--- blog/blog-frontend/src/pages/PostPage.js
+++ blog/blog-frontend/src/pages/PostPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import PostViewer from '../components/post/PostViewer';
 import HeaderContainer from '../containers/common/HeaderContainer';
+import PostViewerContainer from '../containers/post/PostViewerContainer';
 
 const PostPage = () => {
   return (
     <>
       <HeaderContainer />
-      <PostViewer />
+      <PostViewerContainer />
     </>
   );
 };
