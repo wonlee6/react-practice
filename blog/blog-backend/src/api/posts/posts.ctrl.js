ff39807e8d1c43a2dd522b406dd7945aaed9b2cf
wonlee6 <wonlee6@gmail.com>
2020-12-27 07:02:08 +0900

    update 오류 수정, (joi 버전문제)

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,11 +1,10 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
-import Joi from '@hapi/joi';
+import Joi from 'joi';
 import sanitizeHtml from 'sanitize-html';
 
 const { ObjectId } = mongoose.Types;
 
-// 악성스크립트 방지위해 특정 태그들만 허용
 const sanitizeOption = {
   allowedTags: [
     'h1',
@@ -16,6 +15,8 @@ const sanitizeOption = {
     's',
     'p',
     'ul',
+    'ol',
+    'li',
     'blockquote',
     'a',
     'img',
@@ -28,40 +29,17 @@ const sanitizeOption = {
   allowedSchemes: ['data', 'http'],
 };
 
-// {
-//   "title" : "제목",
-//   "body" : "내용",
-//   "tags" : ["태그1", "태그2"]
-// }
-
-// html 없애고 내용이 길면 200자로 제한
-const removeHtmlAndShorten = (body) => {
-  const filtered = sanitizeHtml(body, {
-    allowedTags: [],
-  });
-  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
-};
-
-export const checkOwnPost = (ctx, next) => {
-  const { user, post } = ctx.state;
-  if (post.user._id.toString() !== user._id) {
-    // mongoDB에서 조회한 데이터의 i 값을 문자열과 비교 할때는 반드시 toSTring
-    ctx.status = 403;
-    return;
-  }
-  return next();
-};
-
 export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
   if (!ObjectId.isValid(id)) {
-    ctx.status = 400;
+    ctx.status = 400; // Bad Request
     return;
   }
   try {
     const post = await Post.findById(id);
+    // 포스트가 존재하지 않을 때
     if (!post) {
-      ctx.status = 404;
+      ctx.status = 404; // Not Found
       return;
     }
     ctx.state.post = post;
@@ -69,24 +47,41 @@ export const getPostById = async (ctx, next) => {
   } catch (e) {
     ctx.throw(500, e);
   }
+};
+
+export const checkOwnPost = (ctx, next) => {
+  const { user, post } = ctx.state;
+  if (post.user._id.toString() !== user._id) {
+    ctx.status = 403;
+    return;
+  }
   return next();
 };
 
+/*
+  POST /api/posts
+  {
+    title: '제목',
+    body: '내용',
+    tags: ['태그1', '태그2']
+  }
+*/
 export const write = async (ctx) => {
   const schema = Joi.object().keys({
     // 객체가 다음 필드를 가지고 있음을 검증
-    title: Joi.string().required(), //require가 있으면 필수 항목
+    title: Joi.string().required(), // required() 가 있으면 필수 항목
     body: Joi.string().required(),
     tags: Joi.array().items(Joi.string()).required(), // 문자열로 이루어진 배열
   });
 
-  // 검증 하고 나서 검증 실패인 경우 에러 처리
-  const result = schema.validate(ctx.request.body);
+  // 검증 후, 검증 실패시 에러처리
+  const result = Joi.validate(ctx.request.body, schema);
   if (result.error) {
-    ctx.status = 400;
+    ctx.status = 400; // Bad Request
     ctx.body = result.error;
     return;
   }
+
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
@@ -102,31 +97,41 @@ export const write = async (ctx) => {
   }
 };
 
-// GET  http://localhost:4000/api/posts?username=velopert
-// GET  http://localhost:4000/api/posts?tag=태그
+// html 을 없애고 내용이 너무 길으면 200자로 제한시키는 함수
+const removeHtmlAndShorten = (body) => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  });
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
+};
 
+/*
+  GET /api/posts?username=&tag=&page=
+*/
 export const list = async (ctx) => {
-  // query는 문자 열이기 때문에 숫자로 변환해 주어야 한다.
-  // 값이 주어지지 않았다면 1을 기본으로 설정
+  // query 는 문자열이기 때문에 숫자로 변환해주어야합니다.
+  // 값이 주어지지 않았다면 1 을 기본으로 사용합니다.
   const page = parseInt(ctx.query.page || '1', 10);
 
   if (page < 1) {
     ctx.status = 400;
     return;
   }
+
   const { tag, username } = ctx.query;
   // tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
   const query = {
     ...(username ? { 'user.username': username } : {}),
     ...(tag ? { tags: tag } : {}),
   };
+
   try {
     const posts = await Post.find(query)
-      .sort({ _id: -1 }) // 역순으로 정렬해서 보여줌
-      .limit(10) // 한번에 보이는 숫자 제한
-      .skip((page - 1) * 10) // page 지정 (ex) posts?page=2)
-      .lean() // json형태로 조회
-      .exec(); // exec를 붙여줘야 쿼리 실행
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
     const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
     ctx.body = posts.map((post) => ({
@@ -138,55 +143,60 @@ export const list = async (ctx) => {
   }
 };
 
-export const read = (ctx) => {
+/*
+  GET /api/posts/:id
+*/
+export const read = async (ctx) => {
   ctx.body = ctx.state.post;
-  // const { id } = ctx.params;
-  // try {
-  //   const post = await Post.findById(id).exec();
-  //   if (!post) {
-  //     ctx.status = 404;
-  //     return;
-  //   }
-  //   ctx.body = post;
-  // } catch (e) {
-  //   ctx.throw(500, e);
-  // }
 };
 
+/*
+  DELETE /api/posts/:id
+*/
 export const remove = async (ctx) => {
   const { id } = ctx.params;
   try {
     await Post.findByIdAndRemove(id).exec();
-    ctx.status = 204; // 성공했지만 응답할 데이터는 없음
+    ctx.status = 204; // No Content (성공은 했지만 응답할 데이터는 없음)
   } catch (e) {
     ctx.throw(500, e);
   }
 };
 
+/*
+  PATCH /api/posts/:id
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정', '태그']
+  }
+*/
 export const update = async (ctx) => {
   const { id } = ctx.params;
+  // write 에서 사용한 schema 와 비슷한데, required() 가 없습니다.
   const schema = Joi.object().keys({
     title: Joi.string(),
     body: Joi.string(),
     tags: Joi.array().items(Joi.string()),
   });
 
-  // 검증 실패 시
-  const result = schema.validate(ctx.request.body);
+  // 검증 후, 검증 실패시 에러처리
+  const result = Joi.validate(ctx.request.body, schema);
   if (result.error) {
-    ctx.status = 400;
+    ctx.status = 400; // Bad Request
     ctx.body = result.error;
     return;
   }
+
   const nextData = { ...ctx.request.body }; // 객체를 복사하고
   // body 값이 주어졌으면 HTML 필터링
   if (nextData.body) {
-    nextData.body = sanitizeHtml(nextData, sanitizeOption);
+    nextData.body = sanitizeHtml(nextData.body, sanitizeOption);
   }
   try {
     const post = await Post.findByIdAndUpdate(id, nextData, {
-      new: true, // 이 값을 설정하면 업데이트 된 데이터를 반환
-      // false 업데이트되기 전의 데이터를 반환
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+      // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
     }).exec();
     if (!post) {
       ctx.status = 404;
